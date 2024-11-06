b23d08c4d3a77fb69c8f0b61e53382bc41d06f2c
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-06 11:20:06 +0100

    Create card component and add it to the Admin page on the dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,21 @@
+import UserCard from "@/app/components/UserCard"
+
 const page = () => {
     return (
-      <div></div>
+      <div className="p-4 flex gap-4 flex-col md:flex-row">
+        {/* LEFT */}
+        <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+        <UserCard type = "student"></UserCard>
+        <UserCard type = "teacher"></UserCard>
+        <UserCard type = "parent"></UserCard>
+        <UserCard type = "staff"></UserCard>
+        </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-full lg:w-1/3 bg-red-200">Right</div>
+      </div>
     )
   }
   
