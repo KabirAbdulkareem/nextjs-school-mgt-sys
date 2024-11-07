284184983eceb015f6132def7e30c8e3ae7639a1
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-07 12:30:08 +0100

    Add radial chart to display numbers of male and female students

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,22 +1,35 @@
-import UserCard from "@/components/UserCard"
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
 
 const page = () => {
-    return (
-      <div className="p-4 flex gap-4 flex-col md:flex-row">
-        {/* LEFT */}
-        <div className="w-full lg:w-2/3">
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
-        <UserCard type = "student"></UserCard>
-        <UserCard type = "teacher"></UserCard>
-        <UserCard type = "parent"></UserCard>
-        <UserCard type = "staff"></UserCard>
+          <UserCard type="student"></UserCard>
+          <UserCard type="teacher"></UserCard>
+          <UserCard type="parent"></UserCard>
+          <UserCard type="staff"></UserCard>
         </div>
+        {/* MIDDLE CHART */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
         </div>
-        {/* RIGHT */}
-        <div className="w-full lg:w-1/3 bg-red-200">Right</div>
       </div>
-    )
-  }
-  
-  export default page
+      
+      {/* BOTTOM CHART */}
+      <div className=""></div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 bg-red-200">Right</div>
+    </div>
+  );
+};
+
+export default page;
