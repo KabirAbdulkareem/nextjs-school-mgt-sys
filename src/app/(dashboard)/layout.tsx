d8fcea8fc24dac9b4e46c6359eed28132f934f0f
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-10 14:40:47 +0100

    Add Announcement widget

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -12,14 +12,14 @@ export default function DashboardLayout({
     <div className="h-screen flex">
       {/* Menue-LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 overflow-y-scroll">
-        <Link href="/" className="flex items-center justify-center lg:justify:start gap-2">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
           <Image
             src="/logo.png"
             alt="logo"
             width={32}
             height={32}
           />
-          <span className="hidden lg:block">SchoolRun</span>
+          <span className="hidden lg:block font-bold">School Run</span>
         </Link>
         <Menue />
       </div>
