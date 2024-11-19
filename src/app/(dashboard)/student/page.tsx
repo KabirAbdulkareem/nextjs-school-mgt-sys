5d152ef9d501c3a9eeff173b216f886363657fea
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-19 11:06:02 +0100

    style big calendar on student page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -5,11 +5,11 @@ import EventCalendar from "@/components/EventCalendar";
 
 const page = () => {
   return (
-    <div className="p-4 flex gap-4 flex-col md:flex-row">
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
-      <div className="w-full xl:w-2/3">
+      <div className="w-full xl:w-2/3 ">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semi-bold">Schedule (4A)</h1>
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
           <BigCalendar />
         </div>
       </div>
