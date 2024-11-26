3e3f62664455439e3722e80faa1569a677ff7b99
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-26 17:20:04 +0100

    remove unnecesary code

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -14,12 +14,6 @@ export type Parent = {
   address: string;
 };
 
-export type Column = {
-  header: string;
-  accessor: string;
-  className?: string;
-};
-
 const columns = [
   {
     header: "Info",
@@ -49,36 +43,36 @@ const columns = [
 const ParentListPage = () => {
   const renderRow = (item: Parent) => {
     return (
-        <tr
-          key={item.id}
-          className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-kbPurpleLight"
-        >
-          <td className="flex items-center gap-4 p-4">
-            <div className="flex flex-col">
-              <h3 className="font-semibold">{item.name}</h3>
-              <p className="text-xs text-gray-500">{item?.email}</p>
-            </div>
-          </td>
-          <td className="hidden md:table-cell">{item.students.join(",")}</td>
-          <td className="hidden lg:table-cell">{item.phone}</td>
-          <td className="hidden lg:table-cell">{item.address}</td>
-          <td>
-            <div className="flex items-center gap-2">
-              <Link href={`/list/teachers/${item.id}`}>
-                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-kbSky">
-                  <Image src="/view.png" alt="" width={16} height={16} />
-                </button>
-              </Link>
-              {role === "admin" && (
-                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-kbPurple">
-                  <Image src="/delete.png" alt="" width={16} height={16} />
-                </button>
-              )}
-            </div>
-          </td>
-        </tr>
-      );
-  }
+      <tr
+        key={item.id}
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-kbPurpleLight"
+      >
+        <td className="flex items-center gap-4 p-4">
+          <div className="flex flex-col">
+            <h3 className="font-semibold">{item.name}</h3>
+            <p className="text-xs text-gray-500">{item?.email}</p>
+          </div>
+        </td>
+        <td className="hidden md:table-cell">{item.students.join(",")}</td>
+        <td className="hidden lg:table-cell">{item.phone}</td>
+        <td className="hidden lg:table-cell">{item.address}</td>
+        <td>
+          <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-kbSky">
+                <Image src="/view.png" alt="" width={16} height={16} />
+              </button>
+            </Link>
+            {role === "admin" && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-kbPurple">
+                <Image src="/delete.png" alt="" width={16} height={16} />
+              </button>
+            )}
+          </div>
+        </td>
+      </tr>
+    );
+  };
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
