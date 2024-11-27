56156513e98d3bb9d85505a9fea03388aa2f1f7b
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-27 18:41:18 +0100

    create events list page

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,44 +1,27 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { resultsData, role } from "@/lib/data";
+import { eventsData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-export type Result = {
+export type Event = {
   id: number;
-  subject: string;
+  title: string;
   class: string;
-  teacher: string;
-  student: string;
   date: string;
-  type: "exam" | "assignment";
-  score: number;
+  startTime: string;
+  endTime: string;
 };
 
 const columns = [
   {
-    header: "Subject Name",
-    accessor: "name",
-  },
-  {
-    header: "Student",
-    accessor: "student",
-  },
-  {
-    header: "Score",
-    accessor: "score",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Teacher",
-    accessor: "teacher",
-    className: "hidden md:table-cell",
+    header: "Title",
+    accessor: "title",
   },
   {
     header: "Class",
     accessor: "class",
-    className: "hidden md:table-cell",
   },
   {
     header: "Date",
@@ -46,8 +29,13 @@ const columns = [
     className: "hidden md:table-cell",
   },
   {
-    header: "Type",
-    accessor: "type",
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "End Time",
+    accessor: "endTime",
     className: "hidden md:table-cell",
   },
   {
@@ -56,19 +44,18 @@ const columns = [
   },
 ];
 
-const ResultPage = () => {
-  const renderRow = (item: Result) => {
+const EventListPage = () => {
+  const renderRow = (item: Event) => {
     return (
       <tr
         key={item.id}
         className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-kbPurpleLight"
       >
-        <td className="flex items-center gap-4 p-4">{item.subject}</td>
-        <td>{item.student}</td>
-        <td className="hidden md:table-cell">{item.score}</td>
-        <td className="hidden md:table-cell">{item.teacher}</td>
-        <td className="hidden md:table-cell">{item.class}</td>
+        <td className="flex items-center gap-4 p-4">{item.title}</td>
+        <td>{item.class}</td>
         <td className="hidden md:table-cell">{item.date}</td>
+        <td className="hidden md:table-cell">{item.startTime}</td>
+        <td className="hidden md:table-cell">{item.endTime}</td>
         <td>
           <div className="flex items-center gap-2">
             <Link href={`/list/teachers/${item.id}`}>
@@ -92,7 +79,7 @@ const ResultPage = () => {
       {/* TOP */}
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">
-          All Results
+          All Events
         </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
@@ -112,7 +99,7 @@ const ResultPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={resultsData} />
+      <Table columns={columns} renderRow={renderRow} data={eventsData} />
       {/* PAGINATION */}
 
       <Pagination />
@@ -120,4 +107,4 @@ const ResultPage = () => {
   );
 };
 
-export default ResultPage;
+export default EventListPage;
