3eff7c91cdab10debb9a74d92cc83ea08aefa6bd
KabirAbdulkareem <kabirabdulkareem@gmail.com>
2024-11-29 16:03:45 +0100

    create teacher single page

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.pexels.com" }],
+    remotePatterns: [{ hostname: "images.pexels.com" },{hostname: "media-los2-1.cdn.whatsapp.net"}],
   },
 };
 
